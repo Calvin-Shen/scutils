5b0ca80d6de680c441549892e1b789bf1f6e6167
calvin-git <15050157238@163.com>
2018-12-06 10:46:10 +0800

    Initial Commit

---
+++ libscutils/libscutils.h
@@ -0,0 +1,19 @@
+//
+//  libscutils.h
+//  libscutils
+//
+//  Created by 沈宸 on 2018/12/6.
+//  Copyright © 2018 沈宸. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for libscutils.
+FOUNDATION_EXPORT double libscutilsVersionNumber;
+
+//! Project version string for libscutils.
+FOUNDATION_EXPORT const unsigned char libscutilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <libscutils/PublicHeader.h>
+
+
